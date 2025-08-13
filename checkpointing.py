21d4741891675c12761edfeb7a378d9a69f4596f
a870a <a870a@e230-pc009.inet.dkfz-heidelberg.de>
2025-08-13 19:11:35 +0200

    Avoid errors for checkpoint loading with torch 2.6

--- checkpointing.py
+++ checkpointing.py
@@ -2,7 +2,7 @@ import torch
 
 
 def restore_best_score(callback, ckpt_path):
-    ckpt = torch.load(ckpt_path, map_location="cpu")
+    ckpt = torch.load(ckpt_path, map_location="cpu", weights_only=True)
     callback_state = ckpt["callbacks"].get(type(callback).__qualname__)
     if callback_state and "best_model_score" in callback_state:
         callback.best_model_score = callback_state["best_model_score"]
